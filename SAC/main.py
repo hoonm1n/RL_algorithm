0421a94fd69a015eda360fb203d1d47ebdb2f8c7
hoonm1n <hoonmin1206@gmail.com>
2025-07-30 18:46:11 +0900

    SAC

---
+++ SAC/main.py
@@ -0,0 +1,19 @@
+import gym
+import gymnasium as gym
+import numpy as np
+import torch
+from agent import SAC
+
+
+def main():
+    env = gym.make("Hopper-v4")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    agent = SAC(env, device, gamma=0.99)
+
+    agent.update(total_update_steps=2000000)
+
+    env.close()
+
+if __name__ == "__main__":
+    main()
